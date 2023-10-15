3a04d0d707cb9aff4a51fa29459f16e414613160
MD Soban Jawaid <36644823+sobanjawaid26@users.noreply.github.com>
2023-10-15 20:48:28 +0530

    Time: 2 ms (84.29%), Space: 43.4 MB (78.03%) - LeetHub

--- 2767-maximum-sum-with-exactly-k-elements/2767-maximum-sum-with-exactly-k-elements.java
+++ 2767-maximum-sum-with-exactly-k-elements/2767-maximum-sum-with-exactly-k-elements.java
@@ -5,6 +5,7 @@ class Solution {
             if(max < nums[i])
                 max = nums[i];
         }
+        // 5 + 6 + 7 = 5*3 + 0 + 1 + 2 = summation = n(n+1)/2 but starts from 0 so (n-1)n/2
         return max * k + ((k * (k - 1))/2);
     }
 }
