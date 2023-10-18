7e75baeb603d5309f1bb0527cd27718d0b289ef1
MD Soban Jawaid <36644823+sobanjawaid26@users.noreply.github.com>
2023-10-18 09:54:54 +0530

    Time: 1 ms (78.62%), Space: 41 MB (42.88%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -1,26 +1,16 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        char[] arr1 = word1.toCharArray();
-        char[] arr2 = word2.toCharArray();
-        StringBuilder res = new StringBuilder();
-        int index1 = 0;
-        int index2 = 0;
-
-        while(index1 < arr1.length && index2 < arr2.length){
-            res.append(arr1[index1]);
-            res.append(arr2[index2]);
-            index1++;
-            index2++;        
-
+        StringBuilder result = new StringBuilder();
+        int i = 0;
+        while (i < word1.length() || i < word2.length()) {
+            if (i < word1.length()) {
+                result.append(word1.charAt(i));
+            }
+            if (i < word2.length()) {
+                result.append(word2.charAt(i));
+            }
+            i++;
         }
-        while(index1 < arr1.length){
-            res.append(arr1[index1]);
-            index1++;
-        }
-        while(index2 < arr2.length){
-            res.append(arr2[index2]);
-            index2++;
-        }
-        return res.toString();
+        return result.toString();
     }
 }
