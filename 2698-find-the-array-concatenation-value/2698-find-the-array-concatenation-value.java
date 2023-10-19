eed3596d7e560d8e61fe97d012bc3a7e0585ff73
MD Soban Jawaid <36644823+sobanjawaid26@users.noreply.github.com>
2023-10-19 19:10:36 +0530

    Time: 2 ms (83.14%), Space: 43.2 MB (47.04%) - LeetHub

--- 2698-find-the-array-concatenation-value/2698-find-the-array-concatenation-value.java
+++ 2698-find-the-array-concatenation-value/2698-find-the-array-concatenation-value.java
@@ -1,28 +1,17 @@
 class Solution {
     public long findTheArrayConcVal(int[] nums) {
-        int concatVal = 0;
-        long res = 0;
-
-        while(nums.length > 1){
-            String s1 = Integer.toString(nums[0]); 
-            String s2 = Integer.toString(nums[nums.length - 1]); 
-            String s = s1 + s2;  
-            int c = Integer.parseInt(s); 
-            concatVal = c;
-            res = res + concatVal;
-            nums = removeFirstLast(nums);
+        int low = 0; int high = nums.length - 1; long ans = 0L;
+        StringBuilder temp = new StringBuilder();
+        while(low <= high){
+            if(low != high)
+                temp.append(nums[low]).append(nums[high]);
+            else
+                temp.append(nums[low]);
+            low++;
+            high--;
+            ans += (Integer.parseInt(temp.toString()));
+            temp.setLength(0);
         }
-        if(nums.length == 1)
-            res += nums[0];
-        return res;
-    }
-
-    public static int[] removeFirstLast(int[] arr){
-        int[] r = new int[arr.length - 2];
-        for(int i = 1; i < arr.length - 1; i++){
-            r[i - 1] = arr[i];
-        }
-        return r;
-
+        return ans;
     }
 }
