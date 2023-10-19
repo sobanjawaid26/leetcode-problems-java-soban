ed432177a69bf6feee4f48442c50c10b05c57de1
MD Soban Jawaid <36644823+sobanjawaid26@users.noreply.github.com>
2023-10-19 18:55:48 +0530

    Time: 8 ms (30.47%), Space: 43.8 MB (6.66%) - LeetHub

---
+++ 2698-find-the-array-concatenation-value/2698-find-the-array-concatenation-value.java
@@ -0,0 +1,29 @@
+class Solution {
+    public long findTheArrayConcVal(int[] nums) {
+        int concatVal = 0;
+        long res = 0;
+
+        while(nums.length > 1){
+            String s1 = Integer.toString(nums[0]); 
+            String s2 = Integer.toString(nums[nums.length - 1]); 
+            String s = s1 + s2;  
+            int c = Integer.parseInt(s); 
+            //String s = nums[0] + nums[nums.length];
+            concatVal = c;
+            res = res + concatVal;
+            nums = removeFirstLast(nums);
+        }
+        if(nums.length == 1)
+            res += nums[0];
+        return res;
+    }
+
+    public static int[] removeFirstLast(int[] arr){
+        int[] r = new int[arr.length - 2];
+        for(int i = 1; i < arr.length - 1; i++){
+            r[i - 1] = arr[i];
+        }
+        return r;
+
+    }
+}
