777643adb59227ab7c9a41396d38d1ad177e5882
MD Soban Jawaid <36644823+sobanjawaid26@users.noreply.github.com>
2023-10-25 08:16:41 +0530

    Time: 2 ms (95.98%), Space: 49.1 MB (86.78%) - LeetHub

--- 2631-sort-the-students-by-their-kth-score/2631-sort-the-students-by-their-kth-score.java
+++ 2631-sort-the-students-by-their-kth-score/2631-sort-the-students-by-their-kth-score.java
@@ -1,6 +1,22 @@
 class Solution {
     public int[][] sortTheStudents(int[][] score, int k) {
-        Arrays.sort(score, (a, b) -> b[k] - a[k]);
-        return score;
+        HashMap<Integer,Integer> h=new HashMap<>();
+        int row=score.length;
+        int col=score[0].length;
+
+        int arr[]=new int[row];
+        int ind=0;
+        for(int i=0;i<row;i++){
+            h.put(score[i][k],i);
+            arr[ind++]=score[i][k];
+        }
+        Arrays.sort(arr);
+        int ind1=row-1;
+        int ans[][]=new int[row][col];
+        for(int i=0;i<row;i++){
+            int j=h.get(arr[i]);
+            ans[ind1--]=score[j];
+        }
+        return ans;
     }
 }
